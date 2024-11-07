339d2fc6ddb261460100681cbd6743d5ad393190
Phat <huynhphat@cohota.com>
2024-11-07 17:00:46 +0700

    refactor: update prediction prompt format for improved food identification

--- backend/flask-api/models/gemini_model.py
+++ backend/flask-api/models/gemini_model.py
@@ -6,7 +6,7 @@ genai.configure(api_key=GEMINI_API_KEY)
 
 def get_gemini_prediction(model_name, image):
     img = Image.open(image)
-    prompt = "Please provide the name of the dish shown in this image with format: <dish name>"
+    prompt = "What is this food? Please respond in the following format: Vietnamese: <food_name_vi>, English: <food_name_en>, Ingredients: <ingredient_1> (~<weight_in_grams>), <ingredient_2> (<weight_in_grams>g, ~<calories> kcal),... Total calories: <total_calories> kcal."
     model = genai.GenerativeModel(model_name)
     response = model.generate_content([prompt, img])
 
