d2d077f3dee198e97b4a6eeaeddc22ed956c0229
Phat <huynhphat@cohota.com>
2024-11-07 17:00:08 +0700

    fix: Update test image path for API prediction tests

--- backend/flask-api/test/test_api.py
+++ backend/flask-api/test/test_api.py
@@ -2,20 +2,7 @@ import requests
 
 url = "http://127.0.0.1:5001/predict"
 
-# gemini-1.5-flash-8b': '<Bún riêu> => failed
-# image_path = "images/image1.jpg" Hủ tiếu Nam Vang
-
-# 'predictions_gemini-1.5-flash': 'Com Tam Suon Cha' => very good prediction
-# 'predictions_gemini-1.5-flash-8b': '<Com Tam>' => failed
-# 'predictions_gemini-1.5-pro': 'Cơm tấm sườn nướng' => good prediction 
-# image_path = "images/image6.png"
-
-# 'predictions_gemini-1.5-flash': 'Grilled Fish with Lemon Sauce and Vegetables' => very good prediction
-# 'predictions_gemini-1.5-pro': 'Grilled fish with vegetables' => good prediction
-# 'predictions_model': [{'class': 'scallops', 'score': 0.8646203279495239}, 
-#                       {'class': 'fried_rice', 'score': 0.06544137746095657}, 
-#                       {'class': 'caesar_salad', 'score': 0.011448504403233528}]}
-image_path = "images/image8.png"
+image_path = "images/image1.jpg"
 
 with open(image_path, "rb") as image_file:
     response = requests.post(url, files={"image": image_file})
