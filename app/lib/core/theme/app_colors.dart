467f30e7d6022d1d07b73822ed7a63228a8ec77e
Huynh Tien Phat <2409huynhphat@gmail.com>
2025-02-04 18:25:54 +0700

    feat: add app theme and color definitions for light and dark modes

---
+++ app/lib/core/theme/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // General colors
+  static const Color background = Color(0xffe6eff4);
+  static const Color activeColor = Color(0xff9cdc48);
+  static const Color lightActiveColor = Color(0xffcfff61);
+
+  // Primary colors
+  static const Color primaryGreen = Color(0xff00ae11);
+  static const Color secondaryGreen = Color(0xff9cdc48);
+  static const Color lightGreen = Color(0xFFA5D6A7);
+  static const Color darkGreen = Color(0xFF1B5E20);
+  static const Color accentGreen = Color(0xFF00C853);
+
+  // Secondary colors
+  static const Color white = Colors.white;
+  static const Color lightGray = Color(0xFFF1F1F1);
+  static const Color mediumGray = Color(0xFFBDBDBD);
+  static const Color darkGray = Color(0xFF616161);
+
+  // Status colors
+  static const Color errorRed = Color(0xFFD32F2F);
+  static const Color warningYellow = Color(0xFFFFA000);
+  static const Color successGreen = Color(0xFF388E3C);
+}
