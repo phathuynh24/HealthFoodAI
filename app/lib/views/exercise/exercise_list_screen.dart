cac392bcfcb3a51a503b21e07bf25572283c8a5f
Phat7203 <trantienphat.mx@gmail.com>
2025-02-06 23:50:17 +0700

    feat: add HistoryExercise and ExerciseListScreen components

---
+++ app/lib/views/exercise/exercise_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExerciseListScreen extends StatefulWidget {
+  const ExerciseListScreen({super.key});
+
+  @override
+  State<ExerciseListScreen> createState() => _ExerciseListScreenState();
+}
+
+class _ExerciseListScreenState extends State<ExerciseListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ app/lib/views/exercise/history_exercise.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HistoryExercise extends StatefulWidget {
+  const HistoryExercise({super.key});
+
+  @override
+  State<HistoryExercise> createState() => _HistoryExerciseState();
+}
+
+class _HistoryExerciseState extends State<HistoryExercise> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
